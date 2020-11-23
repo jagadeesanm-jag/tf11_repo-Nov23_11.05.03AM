f763c99d1eea3247203022670d5d009f90f338d3
Jagadeesan Murugesan <jmurugesan@sinmackjemd6m.paloaltonetworks.local>
2020-11-23 11:05:56 +0530

    Modified files:[aws_two_tier.tf]

--- aws_two_tier/aws_two_tier.tf
+++ aws_two_tier/aws_two_tier.tf
@@ -315,3 +315,5 @@ output "FirewallManagementURL" {
 output "WebURL" {
   value = "${join("", list("http://", "${aws_eip.PublicElasticIP.public_ip}"))}"
 }
+
+# Modified [aws_two_tier.tf] on [2020-11-23 11:05:56.866137]
